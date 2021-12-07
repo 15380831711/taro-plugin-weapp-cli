4b8d67f5b25dc2027255d69981aee687772e0a96
15380831711 <15380831711>
2021-12-07 15:54:47 +0800

    '1.0.2'

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,29 @@
 import { exec, which } from 'shelljs';
 const path = require('path');
 
+let isOpen = false;
+
 export default (ctx: any, options: any) => {
     ctx.onBuildFinish(() => {
+        if (isOpen) {
+            return;
+        }
         const platform = ctx.runOpts.platform || ctx.runOpts.options.platform;
         if (platform !== 'weapp') {
             return;
         }
+        const weAppDevToolsPath = options['weApp.devTools.path'];
+        if (weAppDevToolsPath) {
+            const projectPath = path.join(process.cwd(), 'dist');
+            const cliPath = path.join(weAppDevToolsPath, 'cli');
+            const wxCmd = `"${cliPath}" open --project ${projectPath}`;
+            exec(wxCmd);
+            isOpen = true;
+        }
         if (which('xpack-cli')) {
             exec(`xpack-cli open weapp`);
+            isOpen = true;
             return;
         }
-        const weAppDevToolsPath = options['weApp.devTools.path'];
-        if (!weAppDevToolsPath) {
-            return;
-        }
-        const projectPath = path.join(process.cwd(), 'dist');
-        const cliPath = path.join(weAppDevToolsPath, 'cli');
-        const wxCmd = `"${cliPath}" open --project ${projectPath}`;
-        exec(wxCmd);
     });
 };
